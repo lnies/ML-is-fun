1e77e3a6e5d5ca7cab7c952faa4acb273f882a36
Lukas Nies <lnies@uw.edu>
2018-05-25 16:50:01 -0700

    Update

--- MLforXES_Folder/MLforXES/config.py
+++ MLforXES_Folder/MLforXES/config.py
@@ -6,11 +6,6 @@
 from . import *
 
 ### Variables for creating the training, dev, and test sets
-# Distribution parameters
-GAMMA1, SIGMA1, L1 = 0.345, 0.07, 0.18 # parameters of the first voigt profile
-GAMMA2, SIGMA2, L2 = 0.36, 0.08, 0.30 # parameters of the second voigt profile
-MEAN_ENERGY = 2014.00 # in (eV) is mean energy of K_alpha1
-MEAN_ENERGY_SHIFT = 0.85 # in (eV) is mean energy shift between K_alpha1 and K_alpha2
 # Parameters determining size of library and # of spectra
 MAXNUMSTATES = 3
 ## Definition of grids involved
@@ -26,8 +21,8 @@ MAX_ENERGY_FEATURE = 2018.00 # in (eV) maximal value for the feature grid
 GRID_FEATURES = np.arange(MIN_ENERGY_FEATURE, MAX_ENERGY_FEATURE, ENERGY_BIN_WIDTH_FEATURE)
 # Grid for creating the target label
 ENERGY_BIN_WIDTH_TARGET = 0.01 # in (eV) is grid size of discretizing the target grid
-MIN_ENERGY_LABEL = 2012.80 # in (eV) is minimal energy value for target grid
-MAX_ENERGY_LABEL = 2015.20 # in (eV) is maximal energy value for target grid 
+MIN_ENERGY_LABEL = 2013.40 # in (eV) is minimal energy value for target grid
+MAX_ENERGY_LABEL = 2014.80 # in (eV) is maximal energy value for target grid 
 GRID_TARGET = np.arange(MIN_ENERGY_LABEL, MAX_ENERGY_LABEL, ENERGY_BIN_WIDTH_TARGET)
 ## Parameters for splitting the library
 TRAIN_SPLIT = 0.8 # splitting ratio of the library in favor of training set NOT IMPLEMENTED YET
@@ -37,5 +32,9 @@ TEST_SPLIT = 0.1 # splitting ratio of the library in favor of test set NOT IMPLE
 POISSONCNTS = 0.5*1e3 
 ## Other global variables
 legend = ['states', 'energies', 'energies_binned', 'splittings', 'ratios', 'features', 'energy_label', 'plot']
-
 example = 'banana'
+# Distribution parameters
+GAMMA1, SIGMA1, L1 = 0.345, 0.07, 0.18 # parameters of the first voigt profile
+GAMMA2, SIGMA2, L2 = 0.36, 0.08, 0.30 # parameters of the second voigt profile
+RANDOM_ENERGY = np.random.choice(GRID_TARGET) # in (eV) is mean energy of K_alpha1
+MEAN_ENERGY_SHIFT = 0.85 # in (eV) is mean energy shift between K_alpha1 and K_alpha2
